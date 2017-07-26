8553ece283f1cc744e80aa7a53635d993db274fa
Kabir Shah <simplecooldude1@gmail.com>
2017-07-25 17:53:15 -0700

    arch

--- src/slash.asm
+++ src/slash.asm
@@ -10,32 +10,24 @@ SECTION .text
   _slash:
     ; Initialize variables
     mov rax, 1 ; result
-    movsx rcx, byte [rdi] ; current
+    mov rcx, rdi ; current
 
     ; Initial condition
     cmp byte [rcx], 0
     je .return
 
-    ; Store result and prime in 128 bit registers
-    movq xmm0, [prime]
-    movq xmm1, rax
-
     ; Main Loop
     .slashLoop:
       ; Perform carry-less multiplication of result and prime
-      vpclmulqdq xmm1, xmm0, xmm1, 64
+      clm rax, prime
 
       ; XOR result with current
-      movq xmm3, [rcx]
-      pxor xmm1, xmm3
+      xor rax, byte [rcx]
 
       ; Loop logic
       inc rcx
       cmp byte [rcx], 0
       jne .slashLoop
-
-    ; Prepare result for return
-    movq rax, xmm1
 
     ; Return
     .return:
